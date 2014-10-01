4ad538bf9ebfa5b2bc6d83ca6f23191c5dbbd39a
Hristo Tanchev <h.tanchev@emerchantpay.com>
2014-10-01 11:41:03 +0300

    Added deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,32 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'sito_system'
+set :repo_url, 'ssh://hristo@192.168.1.20:91/opt/git/sito_system.git'
+#set :repository, "/home/git/opt/git/sito_system.git"
+#set :local_repository, 'ssh://git@192.168.1.20:91/opt/git/sito_system.git'
 
+#set :user, "hristo" # The user on the VPS server.
+#set :password, "91tan4ev91"
+set :deploy_via, :copy
+#set :use_sudo, true
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/var/www/sito_daily_report'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
-# set :format, :pretty
+set :format, :pretty
 
 # Default value for :log_level is :debug
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+#set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
@@ -35,24 +41,41 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :keep_releases, 5
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+  namespace :assets do
+    task :precompile do
+#       from = source.next_revision(current_revision)
+#       if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+      run_locally("rake assets:clean && rake assets:precompile")
+      run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+      top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+      run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+      run_locally "rm public/assets.tar.bz2"
+      run_locally("rake assets:clean")
     end
   end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
 end
+#       else
+#         logger.info "Skipping asset precompilation because there were no asset changes"
+#       end
+#     end
+#   end
+# end
+
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       # Your restart mechanism here, for example:
+#       # execute :touch, release_path.join('tmp/restart.txt')
+#     end
+#   end
+
+#   after :publishing, :restart
+
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{hristo@192.168.1.20:91}
+role :web, %w{hristo@192.168.1.20:91}
+role :db,  %w{hristo@192.168.1.20:91}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,7 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '192.168.1.20:91', user: 'hristo', roles: %w{web}#, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
@@ -37,3 +37,14 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #     # password: 'please use keys'
 #   }
 # setting per server overrides global ssh_options
+
+# SSH Options
+# See the example commented out section in the file
+# for more options.
+set :ssh_options, {
+  port: 91,
+#  forward_agent: false,
+#  auth_methods: %w(password),
+  password: '91tan4ev91',
+  user: 'hristo',
+}
